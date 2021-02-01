b954e79bf8a9d6a60df45fd8619793af2573b559
Bradley <setokibah@gmail.com>
2021-02-01 13:20:20 +0000

    Added initial dependencies.

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 # Video: https://www.youtube.com/watch?v=xfzGZB4HhEE&t=1s
 
+# Using random data.
+# Does not execute orders at this time.
+
+import numpy as np
+import pandas as pd
+import requests
+import xlsxwriter
+import math
+
